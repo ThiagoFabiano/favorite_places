24d3f8087a792a4f65fd1f87a2e439330b9dbb21
Thiago Fabiano <thiagoffabiano@gmail.com>
2023-07-20 17:44:02 -0300

    feat: permite pegar localização atual do usuario

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/locatio_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -59,6 +60,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ImageInput(onPickImage: (image) {
               _selectedImage = image;
             },),
+            const SizedBox(height: 10,),
+            LocationInput(),
             const SizedBox(height: 16,),
             ElevatedButton.icon(
               onPressed: _savePlace,
