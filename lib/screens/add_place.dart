99ce519aac992b68b3ca49daf32aa71bb7792bc5
Thiago Fabiano <thiagoffabiano@gmail.com>
2023-07-21 14:49:43 -0300

    feat: adiciona localização em texto na listagem e em texto em imagem no detalhar

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/widgets/image_input.dart';
 import 'package:favorite_places/widgets/locatio_input.dart';
 import 'package:flutter/material.dart';
@@ -18,17 +19,17 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
-
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty || _selectedImage == null) {
+    if (enteredTitle.isEmpty || _selectedImage == null || _selectedLocation == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!,_selectedLocation!);
 
     Navigator.of(context).pop();
   }
@@ -61,7 +62,7 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               _selectedImage = image;
             },),
             const SizedBox(height: 10,),
-            LocationInput(),
+            LocationInput(onSelectLocation: (location) {_selectedLocation = location;},),
             const SizedBox(height: 16,),
             ElevatedButton.icon(
               onPressed: _savePlace,
